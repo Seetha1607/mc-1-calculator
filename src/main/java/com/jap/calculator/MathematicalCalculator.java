2135be43fbe7c13b29466f3d86df55f360a6afc5
Seetha <seethalakshmi1607@gmail.com>
2022-09-02 16:14:02 +0530

    logic is executed inside the methods in MathematicalCalculator class

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -2,26 +2,34 @@ package com.jap.calculator;
 
 public class MathematicalCalculator {
 
-	public int add(int num1, int num2)
-	{	
-		return -1;
+	public int add(int num1, int num2) {
+		int sum = num1 + num2;
+		return sum;
 	}
-	public int subtract(int num1, int num2)
-	{	
-		return -2;
-	}
-	public int multiply(int num1, int num2)
-	{	
-		return -1;
-	}
-	public int divide(int num1, int num2)
-	{
-		//write the code and handle the ArithmeticException
-         return -1;
 
+	public int subtract(int num1, int num2) {
+		int sub = num1 - num2;
+		return sub;
 	}
-	public int modulo(int num1, int num2)
-	{	
-		return-1;
+
+	public int multiply(int num1, int num2) {
+		int result = num1 * num2;
+		return result;
+	}
+
+	public int divide(int num1, int num2) {
+		//write the code and handle the ArithmeticException
+		int quotient = 0;
+		try {
+			quotient = num1 / num2;
+		} catch (Exception e) {
+			return 0;
+		}
+		return quotient;
+	}
+
+	public int modulo(int num1, int num2) {
+		int remainder = num1 % num2;
+		return remainder;
 	}
 }
